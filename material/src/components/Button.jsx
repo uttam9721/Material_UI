c2744b64d2cf73f7a007bff69cdf8438004c85e0
uttam9721 <uttammaurya377@gmail.com>
2025-07-26 00:15:41 +0530

    done

--- material/src/components/Button.jsx
+++ material/src/components/Button.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Button, IconButton } from '@mui/material'; // MUI Button
 import AutoDeleteIcon from '@mui/icons-material/AutoDelete';
 // import { IconButton } from '@mui/material';
+import { ButtonGroup } from '@mui/material';
+import { Fab } from '@mui/material';
+import { Checkbox } from '@mui/material';
 
 const MyButton = () => {
   return (
@@ -29,6 +32,49 @@ const MyButton = () => {
      <IconButton color='secondary'>
         <AutoDeleteIcon />
      </IconButton>
+
+     <h1>Custom button</h1>
+     <Button variant="contained" style={{backgroundColor:'green'}} >Text</Button>
+
+     <h1>Click button</h1>
+     <Button variant="contained" onClick={()=>alert('Hii Uttam')} >Click me </Button>
+
+     <h1>button group</h1>
+     <ButtonGroup>
+        <Button>ONE</Button>
+        <Button>TWO</Button>
+        <Button>THREE</Button>
+     </ButtonGroup>
+     <h1>button group vertical</h1>
+     <ButtonGroup orientation='vertical'>
+        <Button>ONE</Button>
+        <Button>TWO</Button>
+        <Button>THREE</Button>
+     </ButtonGroup>
+
+<h1>Floating Action Button</h1>
+     <Fab color='secondary'>
+        U
+     </Fab>
+     <Fab>
+        T
+     </Fab>
+     <Fab>
+        T
+     </Fab>
+     <Fab>
+        A
+     </Fab>
+     <Fab>
+        M
+     </Fab>
+
+     <h1>Checkbox</h1>
+     
+     <Checkbox  defaultChecked />
+<Checkbox control={Checkbox} lebel='start' lebelPlacement="top" />
+<Checkbox  disabled />
+<Checkbox  disabled checked />
     </div>
   );
 };
